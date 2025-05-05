cf0a40aa2336193fb9bf419fb7a504db7db1f6fe
Titiann2147 <alann.samson@gmail.com>
2025-05-05 10:26:30 +0200

    supretion of files

--- game/Faction.js
+++
@@ -1,8 +0,0 @@
-class Faction {
-    constructor(name, units) {
-        this.name = name; // Nom de la faction
-        this.units = units; // Liste des unités disponibles
-    }
-}
-
-export default Faction;
