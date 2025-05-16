0fc50c2b805a84efc3a57a3eac54e39417ade3f0
Arthur Chauvin <103675138+Scartyfy@users.noreply.github.com>
2025-05-16 15:33:34 +0200

    page principale

--- Menu/page1.js
+++ Menu/page1.js
@@ -1,3 +1,4 @@
+
 function startGame() {
     const player1Faction = document.getElementById('player1-faction').value;
     const player2Faction = document.getElementById('player2-faction').value;
@@ -5,3 +6,35 @@ function startGame() {
     // Redirige vers la page de placement des troupes avec les factions sélectionnées
     window.location.href = `Troop_Placement/page2.html?player1Faction=${player1Faction}&player2Faction=${player2Faction}`;
 }
+
+const factions = [
+    { factionName: "rome", img: "image/bouclier_rouge.png" },
+    { factionName: "carthage", img: "image/bouclier_bleu.png" }
+];
+
+let playerSelections = {
+    player1: 0,
+    player2: 0
+};
+
+function updateFactionDisplay(playerId) {
+    const index = playerSelections[playerId];
+    const faction = factions[index];
+    document.getElementById(`${playerId}-image`).src = faction.img;
+}
+
+function nextFaction(playerId) {
+    playerSelections[playerId] = (playerSelections[playerId] + 1) % factions.length;
+    updateFactionDisplay(playerId);
+}
+
+function prevFaction(playerId) {
+    playerSelections[playerId] = (playerSelections[playerId] - 1 + factions.length) % factions.length;
+    updateFactionDisplay(playerId);
+}
+
+function validateFaction(playerId) {
+    const selected = factions[playerSelections[playerId]].factionName;
+    alert(`${playerId} a choisi : ${selected}`);
+}
+
